e1152d3fe8cdb88865a6b7279cf44bc5163638c1
Natthawut Suksai <owen55660@gmail.com>
2024-08-17 20:42:32 +0700

    adding bloc and model

---
+++ lib/bloc/makework_event.dart
@@ -0,0 +1,14 @@
+sealed class MakeWorkEvent{}
+
+class LoadEvent extends MakeWorkEvent{}
+
+class AddEvent extends MakeWorkEvent{
+    final int id;
+    final String userName;
+    final String email;
+    final String firstName;
+    final String lastName;
+    final bool isActive;
+    final DateTime dateJoined;
+    AddEvent(this.id, this.userName, this.email, this.firstName, this.lastName, this.isActive, this.dateJoined);
+}
---
+++ lib/models/makework_models.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+
+class MakeWorkModel extends Equatable {
+  final int id;
+  final String userName;
+  final String email;
+  final String firstName;
+  final String lastName;
+  final bool isActive;
+  final DateTime? dateJoined;
+
+  const MakeWorkModel(
+    this.id, 
+    this.userName, 
+    this.email,
+    this.firstName,
+    this.lastName,
+    this.isActive,
+    this.dateJoined,
+  );
+
+  @override
+  List<Object?> get props => [id, dateJoined];
+} 
